c5132fc7d616230bf241d1914ad50b02ecd20cca
Brenno Baptista <brenno_baptista@hotmail.com>
2021-09-22 23:01:33 -0300

    SellerService with basic FindAll, SellersController and Index View created

---
+++ SalesWebApp/Services/SellerService.cs
@@ -0,0 +1,24 @@
+﻿using SalesWebApp.Data;
+using SalesWebApp.Models.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebApp.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWebAppContext _context;
+
+        public SellerService(SalesWebAppContext contex)
+        {
+            _context = contex;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+
+
+    }
+}
--- SalesWebApp/Startup.cs
+++ SalesWebApp/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SalesWebApp.Data;
+using SalesWebApp.Services;
 
 namespace SalesWebApp
 {
@@ -36,6 +37,7 @@ namespace SalesWebApp
                     builder.MigrationsAssembly("SalesWebApp")));
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
